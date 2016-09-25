9b66e7a934ab21d24793e0eeeb4bb774fdc85989
Pavel <pavelety@gmail.com>
2016-09-25 21:41:49 +0300

    isLocationEnabled true only if GPS is on for elder versions

--- android/plugin/src/main/java/com/tenforwardconsulting/cordova/BackgroundGeolocationPlugin.java
+++ android/plugin/src/main/java/com/tenforwardconsulting/cordova/BackgroundGeolocationPlugin.java
@@ -595,16 +595,17 @@ public class BackgroundGeolocationPlugin extends CordovaPlugin {
     }
 
     public static boolean isLocationEnabled(Context context) throws SettingNotFoundException {
-        int locationMode = 0;
-        String locationProviders;
+        int locationMode;
+//        String locationProviders;
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             locationMode = Settings.Secure.getInt(context.getContentResolver(), Settings.Secure.LOCATION_MODE);
             return locationMode != Settings.Secure.LOCATION_MODE_OFF;
-
         } else {
-            locationProviders = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
-            return !TextUtils.isEmpty(locationProviders);
+            LocationManager locationManager = (LocationManager)context.getSystemService(Context.LOCATION_SERVICE);
+//            locationProviders = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
+//            return !TextUtils.isEmpty(locationProviders);
+            return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
         }
     }
 
